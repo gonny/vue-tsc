c8f96d9096f4f4dd26e297dc44ac0ec9840481ce
Jan Šnobl <snobljan>
2023-11-05 16:14:56 +0100

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,60 @@
-/* eslint-env node */
-//require("@rushstack/eslint-patch/modern-module-resolution");
+/** @type {import('eslint').Linter.FlatConfig[]} */
+//import TSESlintParser from "@typescript-eslint/parser"
+import eslintConfigTypescriptVue from "@vue/eslint-config-typescript/recommended.js"
+import eslintConfigPrettier from "@vue/eslint-config-prettier"
+import eslintPluginVue from "eslint-plugin-vue"
+import eslintTypeScriptPlugin from "@typescript-eslint/eslint-plugin"
+//import rushstackEslintPatchModernModuleResolution from "@rushstack/eslint-patch/modern-module-resolution.js"
+import stylisticPlugin from "@stylistic/eslint-plugin"
+import eslintJs from "@eslint/js"
+import vueEslintParser from "vue-eslint-parser"
+import typeScriptESlintParser from "@typescript-eslint/parser"
+
+import globals from "globals";
 
 export default [
   {
-    root: true,
-    extends: [
-      "eslint:recommended",
-      "@vue/eslint-config-typescript/recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:vue/vue3-recommended",
-      "plugin:vue/base",
-      "@vue/eslint-config-prettier",
-      "@rushstack/eslint-patch/modern-module-resolution"
-    ],
-    "verbatimModuleSyntax": true,
-    parser: "vue-eslint-parser",
-    parserOptions: {
-      ecmaVersion: "latest",
-      parser: "@typescript-eslint/parser",
+    ignores: ["dist/**", "node_modules/**", "bin/**", "build/**", "public/**"],
+  },
+  eslintJs.configs.recommended,
+  eslintTypeScriptPlugin.configs.recommended,
+  eslintConfigTypescriptVue,
+  eslintPluginVue.configs.recommended,
+  eslintPluginVue.configs["vue3-recommended"],
+  eslintConfigPrettier,
+//  rushstackEslintPatchModernModuleResolution,
+  { 
+    files: ["**/*.vue"],
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    languageOptions: {
+      parser: vueEslintParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        parser: typeScriptESlintParser,
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        "vue/setup-compiler-macros": "readonly",
+        //globalVariables
+      },
+      ecmaVersion: 2021,
       sourceType: "module",
     },
-    plugins: ["@stylistic", "vue"],
-    files: ["**/*{.vue,.js,.jsx,.cjs,.mjs,.ts,.tsx,.cts,.mts}"],
-    env: {
-      "vue/setup-compiler-macros": true,
-        browser: true,
-        es2021: true,
-    },
+    plugins: [
+      eslintPluginVue,
+      eslintTypeScriptPlugin,
+      stylisticPlugin
+    ],
+    verbatimModuleSyntax: true,
+    rules: {
+      "stylisticPlugin.rules.quotes": [
+        "error",
+        "single"
+      ],
+    }
   },
 ];
